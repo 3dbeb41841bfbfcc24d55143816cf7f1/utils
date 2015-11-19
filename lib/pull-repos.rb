f59924f1a861627bc8008c0e8aaf2c7f03ea0296
Mike Hopper <drmikeh@gmail.com>
2015-11-19 11:52:03 -0500

    Added archive directory as an ignore for the pull script.

--- lib/pull-repos.rb
+++ lib/pull-repos.rb
@@ -1,7 +1,7 @@
 directory = '.'
 
 Dir.entries(directory).select do |file|
-  if File.directory? file and !(file =='.' || file == '..')
+  if File.directory? file and !(file == '.' || file == '..' || file == 'archive')
     puts "pulling repo #{file}"
     Dir.chdir file
     %x[git pull]
