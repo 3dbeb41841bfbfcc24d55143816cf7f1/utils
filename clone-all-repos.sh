759d693309904289568bae75148874cb2630c274
Mike Hopper <drmikeh@gmail.com>
2015-12-04 17:40:07 -0500

    Added org as a parameter to the clone-all-repos.sh command.

--- clone-all-repos.sh
+++ clone-all-repos.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-ORG_NAME=ATL-WDI-Exercises
+# ORG_NAME=ATL-WDI-Curriculum
+# ORG_NAME=ATL-WDI-Exercises
 
+ORG_NAME=$1
+
+echo "Cloning all repos from $ORG_NAME"
 curl -s https://api.github.com/orgs/${ORG_NAME}/repos?per_page=100 | \
 ruby utils/lib/clone-repos.rb
+
